a24b46dea6af0660a901c625874589ea4d69a206
ssayebee <ssayebee@gmail.com>
2020-01-12 21:31:26 +0900

    Add: page routing
    * '/sign-up'
    * '/sign-in'
    * '/main'
    
    delete file: App.css, App.test.js, index.css, log.svg
    add module: react-router-dom

---
+++ src/constants/models/routes.js
@@ -0,0 +1,4 @@
+export const PATH_SIGNIN = "/sign-in";
+export const PATH_SIGNUP = "/sign-up";
+
+export const PATH_MAIN = "/main";
---
+++ src/route/auth/SignIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignIn = () => {
+  return <div>SignIn page</div>;
+};
+
+export default SignIn;
