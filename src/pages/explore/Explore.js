bb1332f72f2d1097984fc143875ff8dc027615d1
ssayebee <ssayebee@gmail.com>
2020-02-29 14:46:15 +0900

    Incomplete Infinit Scroll

--- src/pages/explore/Explore.js
+++ src/pages/explore/Explore.js
@@ -1,6 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography, Grid, Paper } from "@material-ui/core"
+import { Typography, Grid, Paper, Card, Avatar, Chip, CardContent } from "@material-ui/core"
+import { api } from "api";
+import { __POSITIONS } from "constants/values";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,39 +17,101 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+
 const Explore = props => {
+  let loadable = true;
+  let isLoading = false;
+  let loadUrl = 'accounts';
   const classes = useStyles();
   const [target, setTarget] = useState(null);
-  const [card, setCard] = useState([1]);
+  const [accounts, setAccounts] = useState(null);
+
+  const getAccounts = async () => {
+    isLoading = true
+
+    try {
+      const res = await api.get(loadUrl);
+      if (res.status === 200) {
+        const responseData = res.data._embedded.accountResponseDtoList
+        console.log(accounts);
+        
+        if (accounts === null) {
+          console.log("처음 로딩");
+          setAccounts(responseData)
+        } else {
+          console.log("나중 로딩");
+          setAccounts(accounts.concat(responseData))
+        }
+        if (res.data._links.next) {
+          console.log("more data");
+          loadUrl = res.data._links.next.href
+          loadUrl = loadUrl.replace("http", "https")
+          loadable = true
+        } else {
+          console.log("no more data");
+          loadable = false
+        }
+
+        isLoading = false;
+      }
+    } catch (err) {
+      loadable = false;
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
     let observer;
     if (target) {
-      observer = new IntersectionObserver(onIntersect, {threshold: 0.5});
+      observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
       observer.observe(target);
     }
     return () => observer && observer.disconnect();
-  }, [target, card]);
+  }, [target]);
 
-  const onIntersect = ([entry], observer) => {
-    if (entry.isIntersecting){
+  const handleIntersection = async ([entry], observer) => {
+    if (entry.isIntersecting) {
       observer.unobserve(entry.target);
-      setCard(card.concat([1, 1, 1, 1]));
+      if (loadable && !isLoading) {
+        console.log("load data");
+        await getAccounts();
+      }
       observer.observe(entry.target);
     }
   }
 
   return (
     <div className={classes.root}>
-      <Typography variant="h3">Explore</Typography>
       <Grid container spacing={3} xs={12}>
-        {card.map(num => (
-          <Grid item xs={12} md={4}>
-            <Paper className={classes.paper}>card</Paper>
+        {accounts && accounts.map(account => (
+          <Grid item xs={12} md={4} key={account.id}
+            onClick={
+              () => props.history.push(`resume/${account.id}`)
+            }
+          >
+            <Card className={classes.card}>
+              <div style={{ display: 'flex', padding: 20 }}>
+                <Avatar style={{ width: 120, height: 120 }} src={account.image}></Avatar>
+                <div style={{ textAlign: 'none', marginLeft: 20 }}>
+                  <Typography variant="h6" style={{ marginBottom: 10 }}>{account.nickName}</Typography>
+                  {account.positions.map(position => {
+                    return (`${__POSITIONS[position]}, `)
+                  })}
+                  <div style={{ marginBottom: 10 }} />
+                  {account.technologies.map(tech => {
+                    return (<Chip style={{ marginRight: 10, marginBottom: 10 }} label={tech.name} />)
+                  })}
+                </div>
+              </div>
+              <CardContent>
+                {account.oneLineIntroduce}
+                <Typography style={{ textAlign: 'right' }} variant="subtitle2">{`조회수 ${account.hits} 좋아요 ${account.favoriteCount}`}</Typography>
+              </CardContent>
+            </Card>
           </Grid>
         ))}
       </Grid>
-      <div ref={setTarget}>waiting...</div>
+      <div ref={setTarget} style={{ marginTop: 20 }}>데이터가 없습니다.</div>
     </div>
   );
 };
