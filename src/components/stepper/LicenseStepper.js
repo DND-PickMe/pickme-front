7a5167568936191d263c250258b817f8e321f65f
ssayebee <ssayebee@gmail.com>
2020-03-06 21:06:48 +0900

    Add Profile stepper

---
+++ src/components/stepper/LicenseStepper.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    height: 50,
+    paddingLeft: theme.spacing(4),
+    backgroundColor: theme.palette.background.default,
+  },
+}));
+
+export default ({ items, ...rest }) => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = items.length;
+
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
+
+  return (
+    items.length > 0 ?
+      <div className={classes.root}>
+        <Paper elevation={3} style={{ padding: 20, marginBottom: 20 }}>
+          <Typography variant="subtitle1" style={{ marginBottom: 20 }}>{items[activeStep].name}</Typography>
+          <Typography variant="subtitle2" style={{ marginBottom: 20 }}>{items[activeStep].institution}</Typography>
+          <Typography variant="subtitle2" style={{ marginBottom: 20 }}>{items[activeStep].description}</Typography>
+        </Paper>
+
+        <MobileStepper
+          steps={maxSteps}
+          position="static"
+          variant="text"
+          activeStep={activeStep}
+          nextButton={
+            <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+              Next
+            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </Button>
+          }
+          backButton={
+            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+              {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+              Back
+          </Button>
+          }
+        />
+      </div>
+      :
+      <div />
+  );
+}
--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField/TextField";
-import { Button, makeStyles, Paper, Grid } from "@material-ui/core";
+import { Button, makeStyles, Paper, Grid, Hidden } from "@material-ui/core";
 import PasswordInput from "components/PasswordInput";
 import { api } from "api";
 import Swal from 'sweetalert2';
@@ -12,7 +12,7 @@ const useStyles = makeStyles(theme => ({
     flexWrap: "wrap"
   },
   activityTab: {
-    padding: 12,
+    padding: 8,
     borderBottom: 'none',
     backgroundColor: 'white',
     borderBottomLeftRadius: 0,
@@ -20,7 +20,7 @@ const useStyles = makeStyles(theme => ({
     fontWeight: 'bold',
   },
   unActivityTab: {
-    padding: 12,
+    padding: 8,
     borderBottom: 'none',
     backgroundColor: 'none'
   }
@@ -31,6 +31,8 @@ const SignUp = props => {
   const [isUser, setIsUser] = useState(true);
   const [userInputs, setUserInputs] = useState({});
   const [epInputs, setEpInputs] = useState({});
+  const [vaildCode, setValidCode] = useState();
+  const [visiable, setVisiable] = useState(false);
 
   const userChange = prop => event => {
     setUserInputs({ ...userInputs, [prop]: event.target.value });
@@ -65,11 +67,11 @@ const SignUp = props => {
         })
         props.history.push(PATH_SIGN_IN);
       }
-      
+
     } catch (err) {
       const messages = [];
       err.response.data.map(message => messages.
-      push(`<ul>${message}</ul>`));
+        push(`<ul>${message}</ul>`));
 
       Swal.fire({
         icon: 'error',
@@ -97,7 +99,7 @@ const SignUp = props => {
     } catch (err) {
       const messages = [];
       err.response.data.map(message => messages.
-      push(`<ul>${message}</ul>`));
+        push(`<ul>${message}</ul>`));
 
       Swal.fire({
         icon: 'error',
@@ -108,47 +110,118 @@ const SignUp = props => {
   }
 
   return (
-    <Grid>
-      <div style={{ marginTop: 30 }} />
-      <Button className={isUser ? classes.activityTab : classes.unActivityTab} onClick={() => setIsUser(true)}>일반 회원</Button>
-      <Button className={!isUser ? classes.activityTab : classes.unActivityTab} onClick={() => setIsUser(false)}>기업 회원</Button>
-      {isUser ?
-        <Paper elevation={3} style={{ padding: 30 }}>
-          <h2>일반 회원가입</h2>
-          <TextField onChange={userChange("email")} fullWidth label="E-mail" variant="outlined" />
-          <div style={{ marginBottom: 20 }} />
-          <PasswordInput label="비밀번호" onChange={userChange("password")} />
-          <div style={{ marginBottom: 20 }} />
-          <PasswordInput label="비밀번호 확인" labelWidth={100} onChange={userChange("passwordConfirm")} />
-          <div style={{ marginBottom: 20 }} />
-          <TextField onChange={userChange("nickName")} fullWidth label="이름" variant="outlined" />
-          <div style={{ marginBottom: 20 }} />
-          <TextField onChange={userChange("oneLineIntroduce")} fullWidth label="한줄소개" variant="outlined" />
-          <div style={{ marginBottom: 20 }} />
-          <Button onClick={handleSubmitUser}>Submit</Button>
-        </Paper>
-        :
-        <Paper elevation={3} style={{ padding: 30 }}>
-          <h2>기업 회원가입</h2>
-          <TextField onChange={epChange("email")} fullWidth label="E-mail" variant="outlined" />
-          <div style={{ marginBottom: 20 }} />
-          <PasswordInput label="비밀번호" onChange={epChange("password")} />
-          <div style={{ marginBottom: 20 }} />
-          <PasswordInput label="비밀번호 확인" labelWidth={100} onChange={epChange("passwordConfirm")} />
-          <div style={{ marginBottom: 20 }} />
-          <TextField onChange={epChange("registrationNumber")} fullWidth label="사업자번호" variant="outlined" />
-          <div style={{ marginBottom: 20 }} />
-          <TextField onChange={epChange("name")} fullWidth label="회사명" variant="outlined" />
-          <div style={{ marginBottom: 20 }} />
-          <TextField onChange={epChange("address")} fullWidth label="회사 주소" variant="outlined" />
-          <div style={{ marginBottom: 20 }} />
-          <TextField onChange={epChange("ceoName")} fullWidth label="대표명" variant="outlined" />
-          <div style={{ marginBottom: 20 }} />
-          <Button onClick={handleSubmitEp}>Submit</Button>
-        </Paper>
-      }
+    <div style={{ display: "flex", marginTop: 20 }}>
+      <Hidden xsDown >
+        <div style={{ flex: 1 }} />
+      </Hidden>
+      <div style={{ flex: 2 }}>
+        <Button className={isUser ? classes.activityTab : classes.unActivityTab} onClick={() => setIsUser(true)}>일반 회원</Button>
+        <Button className={!isUser ? classes.activityTab : classes.unActivityTab} onClick={() => setIsUser(false)}>기업 회원</Button>
+        {isUser ?
+          <Paper
+            style={{
+              borderTopLeftRadius: 'inherit',
+              padding: 30
+            }}>
+            <h2>일반 회원가입</h2>
+            <div style={{ display: 'flex' }}>
+              <TextField
+                style={{ flex: 10 }}
+                onChange={userChange("email")}
+                label="E-mail"
+                variant="outlined" />
+              <Button
+                style={{ marginLeft: 12, flex: 2, height: 56 }}
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  api.post("api/accounts/sendCode", { email: userInputs.email }).then(res => {
+                    alert("해당 이메일로 인증번호가 전송되었습니다.")
+                    setVisiable(true)
+                  }).catch(err => {
+                    alert("서버에 문제가 있습니다. 잠시 후 다시 시도해 주세요.")
+                  })
+                }
+                }>인증 코드 전송</Button>
+            </div>
+            <div style={{ marginBottom: 20 }} />
+            <div style={{ display: 'flex' }}>
+              {visiable &&
+                <>
+                  <TextField
+                    style={{ flex: 10 }}
+                    onChange={e => setValidCode(e.target.value)}
+                    label="인증번호" variant="outlined" />
+                  <Button
+                    style={{ marginLeft: 12, flex: 2, height: 56 }}
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
 
-    </Grid>
+                      api.code("api/accounts/matchCode", { email: userInputs.email, code: vaildCode })
+                        .then(res => {
+                          Swal.fire({
+                            icon: 'success',
+                            title: '이메일이 인증이 완료되었습니다.',
+                            showConfirmButton: false,
+                            timer: 1500
+                          })
+                        }).catch(err => {
+                          const messages = [];
+                          err.response.data.map(message => messages.
+                            push(`<ul>${message}</ul>`));
+
+                          Swal.fire({
+                            icon: 'error',
+                            title: '인증 실패',
+                            html: messages.join('\n')
+                          })
+                        })
+                    }}
+                  > 인증</Button>
+                  <div style={{ marginBottom: 20 }} />
+                </>
+              }
+            </div>
+            <PasswordInput label="비밀번호" onChange={userChange("password")} />
+            <div style={{ marginBottom: 20 }} />
+            <PasswordInput label="비밀번호 확인" labelWidth={100} onChange={userChange("passwordConfirm")} />
+            <div style={{ marginBottom: 20 }} />
+            <TextField onChange={userChange("nickName")} fullWidth label="이름" variant="outlined" />
+            <div style={{ marginBottom: 20 }} />
+            <TextField onChange={userChange("oneLineIntroduce")} fullWidth label="한줄소개" variant="outlined" />
+            <div style={{ marginBottom: 20 }} />
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Button onClick={handleSubmitUser}>Submit</Button>
+            </div>
+          </Paper>
+          :
+          <Paper elevation={3} style={{ padding: 30 }}>
+            <h2>기업 회원가입</h2>
+            <TextField onChange={epChange("email")} fullWidth label="E-mail" variant="outlined" />
+            <div style={{ marginBottom: 20 }} />
+            <PasswordInput label="비밀번호" onChange={epChange("password")} />
+            <div style={{ marginBottom: 20 }} />
+            <PasswordInput label="비밀번호 확인" labelWidth={100} onChange={epChange("passwordConfirm")} />
+            <div style={{ marginBottom: 20 }} />
+            <TextField onChange={epChange("registrationNumber")} fullWidth label="사업자번호" variant="outlined" />
+            <div style={{ marginBottom: 20 }} />
+            <TextField onChange={epChange("name")} fullWidth label="회사명" variant="outlined" />
+            <div style={{ marginBottom: 20 }} />
+            <TextField onChange={epChange("address")} fullWidth label="회사 주소" variant="outlined" />
+            <div style={{ marginBottom: 20 }} />
+            <TextField onChange={epChange("ceoName")} fullWidth label="대표명" variant="outlined" />
+            <div style={{ marginBottom: 20 }} />
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Button onClick={handleSubmitEp}>Submit</Button>
+            </div>
+          </Paper>
+        }
+      </div>
+      <Hidden xsDown >
+        <div style={{ flex: 1 }} />
+      </Hidden>
+    </div >
 
   );
 };
